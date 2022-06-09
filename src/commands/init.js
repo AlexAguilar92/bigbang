2df35109b063cc35cf3022bf9baf0019de0be702
Alex Aguilar <auza920205@gmail.com>
2022-06-09 09:22:48 -0500

    fixed init file

--- src/commands/init.js
+++ src/commands/init.js
@@ -1,7 +1,7 @@
 const init = () => {
   // console.log('init')
-  process.stdout.write('init')
-  process.stdout.write('init2 \r')
+  process.stdout.write('init \r')
+  process.stdout.write('init2')
 }
 
 module.exports = init
