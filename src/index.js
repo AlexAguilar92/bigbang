3654a92fa3afad2533e37d8bc336b54e8fcafa2c
Alexandro Aguilar <aleaguilar@palaceresorts.com>
2022-06-09 23:32:00 -0500

    added new command

--- src/index.js
+++ src/index.js
@@ -11,4 +11,11 @@ program
   .option('-n, --name <name>', 'The name of the project')
   .action((options) => init(options.name))
 
+program
+  .command('create')
+  .description('Creates a new resource depending on the type')
+  .requiredOption('-t, --type <type>', 'The type of the resource (required)')
+  .requiredOption('-n, --name <name>', 'The name of the resource (required)')
+  .action((options) => console.log(options))
+
 program.parse()
