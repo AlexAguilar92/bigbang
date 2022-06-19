627b9908bd1e181cfb529e903499d03cb6c989bb
Alexandro Aguilar <aleaguilar@palaceresorts.com>
2022-06-19 15:39:11 -0500

    package added

--- src/domain/entity/service.js
+++ src/domain/entity/service.js
@@ -2,7 +2,8 @@ import fs from 'fs/promises'
 
 const service = {
   create: async (name, path) => {
-    console.log('creating service...', name, path)
+    // console.log('creating service...', name, path)
+    const createService = await fs.writeFile(`${path}/${name}.js`, `const ${name} = {};\n\nexport default ${name};`)
   }
 }
 
