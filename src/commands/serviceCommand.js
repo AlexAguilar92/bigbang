617dbf76d71a988d49865a89e12f10e0db1b012f
Alexandro Aguilar <aleaguilar@palaceresorts.com>
2022-06-25 21:09:12 -0500

    disabled logs

--- src/commands/serviceCommand.js
+++ src/commands/serviceCommand.js
@@ -5,7 +5,7 @@ const service = async (options) => {
   // const spinner = ora('creating service...').start()
   const { result, error } = await serviceAdapter(options)
   if (result === 0) {
-    spinner.succeed('Service created')
+    //spinner.succeed('Service created')
   } else {
     // spinner.fail(`Error creating resource, ${error}`)
     // console.error(error)
