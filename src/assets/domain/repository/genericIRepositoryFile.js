f5a8a7f6b029f78984731dd0ab193ee2b5c3c9c1
AlanCodster <alan@codster.io>
2022-07-09 09:41:24 -0500

    feature: fix imports and configuration at generic service and repository

--- src/assets/domain/repository/genericIRepositoryFile.js
+++ src/assets/domain/repository/genericIRepositoryFile.js
@@ -1,11 +1,11 @@
 const iRepositoryFile = (name) => (
-  `import { IRead, IWrite } from "../..";
-  import { ${name.charAt(0).toUpperCase()}${name.substring(1)} } from "../";
-  
-  export default interface I${name.charAt(0).toUpperCase()}${name.substring(1)}Repository extends IWrite<User>, IRead<User> {
-  
-  }
-  `
-  )
+  `import IWrite from '../../../../common/domain/repository/interface/IWrite';
+  import IRead from '../../../../common/domain/repository/interface/IRead';
 
-  export default iRepositoryFile
+  import ${name.charAt(0).toUpperCase()}${name.substring(1)} from '../../entity/implementation/${name.charAt(0).toUpperCase()}${name.substring(1)}';
+  
+  export default interface I${name.charAt(0).toUpperCase()}${name.substring(1)}Repository extends IWrite<${name.charAt(0).toUpperCase()}${name.substring(1)}>, IRead<${name.charAt(0).toUpperCase()}${name.substring(1)}> {}
+  `
+)
+
+export default iRepositoryFile
