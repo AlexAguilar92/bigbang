5c59646dc8f3fc0b1e80397fd790080d4fac8a75
Alex Aguilar <auza920205@gmail.com>
2022-07-12 16:32:32 -0500

    fixed repository

--- src/assets/domain/repository/genericIRepositoryFile.js
+++ src/assets/domain/repository/genericIRepositoryFile.js
@@ -2,8 +2,6 @@ const iRepositoryFile = (name) => (
 `import IWrite from '../../../../common/domain/repository/interface/IWrite';
 import IRead from '../../../../common/domain/repository/interface/IRead';
 
-import ${name.charAt(0).toUpperCase()}${name.substring(1)} from '../../entity/implementation/${name.charAt(0).toUpperCase()}${name.substring(1)}';
-
 export default interface I${name.charAt(0).toUpperCase()}${name.substring(1)}Repository extends IWrite<${name.charAt(0).toUpperCase()}${name.substring(1)}>, IRead<${name.charAt(0).toUpperCase()}${name.substring(1)}> {}
 `
 )
